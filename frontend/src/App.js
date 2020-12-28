2df5a84f08dc1d01c2fa039bb842400ef81283d5
kekkerslawl <braydj@gmail.com>
2020-12-27 21:13:06 -0600

    working on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <Footer />
     </>
   );
 };
